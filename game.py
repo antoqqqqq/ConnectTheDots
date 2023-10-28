4c31d9ae382f0f391c35b25210228694f9a07d60
KhaiDucDuong <greenjohnny135@gmail.com>
2023-10-28 23:19:00 +0700

    create a simple menu  (will delete later)
    this menu in game.py is to test features only, I'll separate the codes to menu file later

--- game.py
+++ game.py
@@ -1,2 +1,27 @@
 # this file will create the game and run it
 # don't code in this file yet!!
+import pygame
+from sprite import Label
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Main Menu")
+
+game_paused = False
+menu_state = "main"
+
+label1 = Label(200, 200, "Connect the Dots", font_size=50, color=(206, 90, 103))
+run = True
+
+while run:
+    screen.fill((52, 78, 91))
+    label1.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
