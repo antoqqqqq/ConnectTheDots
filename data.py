87fd5250ec91c1b5a3be93f3cbdbf61405bbc395
antoqqqqq <nguyenphuochaha@gmail.com>
2023-11-04 16:45:52 +0700

    update data processing

--- data.py
+++ data.py
@@ -1,20 +1,39 @@
 def readfile(filename):
     try:
         with open(filename, 'r') as f:
-            contents = f.read()
-            for line in contents:
-                elements=line.split('-')
-                stage = elements[0]
-                number_of_move =elements[1]
-                number_of_turns =elements[2]
-                time =elements[3]
+            data_entries = []
+            for line in f:
+                #row(stage, number of moves, number of turns, time)
+                row = []
+                elements=line.strip('\n').split('-')
+                row.append(elements[0])
+                row.append(elements[1])
+                row.append(elements[2])
+                row.append(elements[3])
+
+                data_entries.append(row)
+        return data_entries
     except IOError:
         print("Error: could not read file " + filename)
 def write_file(filename,text):
     try:
+        element=text.split('-')
+        data=[]
+        data=readfile(filename)
+        i=0
+
+
+        for row in data:
+            if (element[0]== row[0]):
+                data[i][0] = element[0]
+                data[i][1] = element[1]
+                data[i][2] = element[2]
+                data[i][3] = element[3]                                                
+                break
+            i=+1
         with open(filename, 'w') as f:
-            f.write(text)
+            for row in data:
+                f.write(row[0]+'-'+row[1]+'-'+row[2]+'-'+row[3]+'\n')
     except IOError:
         print("Error: could not write file " + filename)
-
-    
+write_file('text.txt','1-2-3-4')
