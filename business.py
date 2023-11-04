d410b237f6139249d648aec0c49126ff05bf1541
TuanHuuTran <tuanhuutran779@gmail.com>
2023-11-04 10:17:42 +0700

    creat class Dot Tile Board
    creat Dot, Tile and Board

---
+++ business.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+class Dot:
+    def __init__(self, value : int , color : str, radius : int )
+        self.value = value
+        self.color = color
+        self.radius = radius
+        pass
+class Tile:
+    def __init__(self, length : int , line_enter_direction : str,line_exit_direction : str, line_color = None  dot : Optional ['Dot'] = None)
+        self.length = length
+        self.dot = dot
+        self.line_enter_direction = 
+        self.line_exit_diẻction = None
+
+
+        pass
